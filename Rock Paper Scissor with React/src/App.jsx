530148f1be715e10344871ce86550c8c4f425e58
trilokadhikari <tradhikari062@gmail.com>
2026-07-06 16:16:09 +0545

    Add main App component to initialize the Rock-Paper-Scissors game

---
+++ Rock Paper Scissor with React/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Component  from './Component.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Component />
+    </>
+  )
+}
+
+export default App
